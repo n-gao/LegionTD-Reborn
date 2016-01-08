ef38269f21ca8b8ac0b40a8046b094167c0dfd25
funcdoor <srkingdavy@gmail.com>
2016-01-08 09:11:13 -0800

    bleh

--- game/dota_addons/legion_td/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/legion_td/scripts/vscripts/addon_game_mode.lua
@@ -120,28 +120,28 @@ RequiredUnits = {
 	"tower_elementalbuilder_waterwarrior",
 
 	--Humanbuilder
-	"tower_humanbuilder_archbishop"
-	"tower_humanbuilder_archer"
-	"tower_humanbuilder_archmage"
-	"tower_humanbuilder_blademaster"
-	"tower_humanbuilder_footman"
-	"tower_humanbuilder_futuristic_gyrocopter"
-	"tower_humanbuilder_general"
-	"tower_humanbuilder_gunner"
-	"tower_humanbuilder_gyrocopter_mk1"
-	"tower_humanbuilder_gyrocopter_mk2"
-	"tower_humanbuilder_hunter"
-	"tower_humanbuilder_icewrack_grandmaster"
-	"tower_humanbuilder_lieutenant"
-	"tower_humanbuilder_mage"
-	"tower_humanbuilder_marksman"
-	"tower_humanbuilder_militia"
-	"tower_humanbuilder_novice"
-	"tower_humanbuilder_paladin"
-	"tower_humanbuilder_sharpshooter"
-	"tower_humanbuilder_soldier"
-	"tower_humanbuilder_soundmaster"
-	"tower_humanbuilder_spearman"
+	"tower_humanbuilder_archbishop",
+	"tower_humanbuilder_archer",
+	"tower_humanbuilder_archmage",
+	"tower_humanbuilder_blademaster",
+	"tower_humanbuilder_footman",
+	"tower_humanbuilder_futuristic_gyrocopter",
+	"tower_humanbuilder_general",
+	"tower_humanbuilder_gunner",
+	"tower_humanbuilder_gyrocopter_mk1",
+	"tower_humanbuilder_gyrocopter_mk2",
+	"tower_humanbuilder_hunter",
+	"tower_humanbuilder_icewrack_grandmaster",
+	"tower_humanbuilder_lieutenant",
+	"tower_humanbuilder_mage",
+	"tower_humanbuilder_marksman",
+	"tower_humanbuilder_militia",
+	"tower_humanbuilder_novice",
+	"tower_humanbuilder_paladin",
+	"tower_humanbuilder_sharpshooter",
+	"tower_humanbuilder_soldier",
+	"tower_humanbuilder_soundmaster",
+	"tower_humanbuilder_spearman",
 	"tower_humanbuilder_tactician"
 }
 
