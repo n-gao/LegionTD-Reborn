b708847280451476b6932b714d71077068ac5289
funcdoor <srkingdavy@gmail.com>
2016-01-08 12:14:34 -0800

    Gave bosses a passive self-repel

---
+++ game/dota_addons/legion_td/scripts/vscripts/abilities/waveunits/waveboss_repel.lua
@@ -0,0 +1,27 @@
+function RepelResetter( keys )
+	print ("waveboss_repel.lua")
+	local caster = keys.caster
+	local caster_location = caster:GetAbsOrigin()
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+	local cooldown = ability:GetCooldown(ability_level)
+	local modifier_time = keys.timemodifier
+	local modifier_spell = keys.spellmodifier
+	print ("locals")
+
+	ability:StartCooldown(cooldown)
+	caster:RemoveModifierByName(modifier_time)
+	print ("mod removed")
+
+	ability:ApplyDataDrivenModifier(caster, caster, modifier_spell, {})
+
+	print ("spell applied")
+
+	Timers:CreateTimer(cooldown, function()
+		ability:ApplyDataDrivenModifier(caster, caster, modifier_time, {})
+	end)
+
+	print ("timer created")
+	
+end
+
