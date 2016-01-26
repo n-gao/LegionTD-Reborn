c7b8870beedd7eff501fc7d1a2e3ec26c98b7a0d
funcdoor <srkingdavy@gmail.com>
2016-01-25 19:08:38 -0800

    fix copypasta fail

--- game/dota_addons/legion_td/scripts/vscripts/gamespawner.lua
+++ game/dota_addons/legion_td/scripts/vscripts/gamespawner.lua
@@ -238,7 +238,7 @@ function GameSpawner:SendIncomingUnits(team)
           table.insert(unit.waypoints, lane.waypoints[j] + positions[k])
         end
 
-        unit.waypoints[4].y = value.waypoints[4].y -- make final waypoint y aligned with king regardless of formation rank
+        unit.waypoints[4].y = unit.waypoints[4].y -- make final waypoint y aligned with king regardless of formation rank
         unit.wayStep = 2
 
         ExecuteOrderFromTable({
