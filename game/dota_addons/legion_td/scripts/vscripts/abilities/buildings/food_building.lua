503c36b1beb5893967bb3185cf21c4cc28219666
funcdoor <srkingdavy@gmail.com>
2016-02-06 17:16:02 -0800

    Food Limit can now only be upgraded 12 times

--- game/dota_addons/legion_td/scripts/vscripts/abilities/buildings/food_building.lua
+++ game/dota_addons/legion_td/scripts/vscripts/abilities/buildings/food_building.lua
@@ -3,4 +3,9 @@ function increase_food_limit(event)
   local player = event.caster.player
   player:IncreaseFoodLimit(increment)
   player.buildingUpgradeValue = player.buildingUpgradeValue + event.ability:GetGoldCost(-1)
+  if event.ability:GetLevel() == event.ability:GetMaxLevel() then
+    event.caster:RemoveAbility(event.ability:GetAbilityName())
+  else
+    event.ability:SetLevel(event.ability:GetLevel() + 1)
+  end
 end
