e63887cb0176f32c8c9b5f1dae475e8b02a23b39
Roofkiller <nicholas.gao96@gmail.com>
2016-09-14 21:54:52 +0200

    fixed precache bug

--- game/dota_addons/legion_td/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/legion_td/scripts/vscripts/addon_game_mode.lua
@@ -1,4 +1,5 @@
 -- Required files to be visible from anywhere
+require ('precache')
 require ('game_settings')
 require ('timers')
 require ('commands')
@@ -15,8 +16,26 @@ require ('notifications')
 require ('damage')
 require ('selection')
 require ('wiki')
-require ('precache')
 require ('wave_unit')
+
+function Precache( context )
+	-- NOTE: IT IS RECOMMENDED TO USE A MINIMAL AMOUNT OF LUA PRECACHING, AND A MAXIMAL AMOUNT OF DATADRIVEN PRECACHING.
+	-- Precaching guide: https://moddota.com/forums/discussion/119/precache-fixing-and-avoiding-issues
+
+	--[[
+	This function is used to precache resources/units/items/abilities that will be needed
+	for sure in your game and that cannot or should not be precached asynchronously or
+	after the game loads.
+
+	See GameMode:PostLoadPrecache() in barebones.lua for more information
+	]]
+
+	print("[BAREBONES] Performing pre-load precache")
+
+	for _,r in pairs(RequiredUnits) do
+		PrecacheUnitByNameSync(r, context)
+	end
+end
 
 -- Create the game mode when we activate
 function Activate()
