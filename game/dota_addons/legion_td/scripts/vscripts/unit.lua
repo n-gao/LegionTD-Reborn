cdad423243adf77a2e355d3f99bd4d440a3d1fd0
Doyle3694 <Doyle3694@users.noreply.github.com>
2016-09-23 18:20:57 +0200

    Removed astralbuilder from master

--- game/dota_addons/legion_td/scripts/vscripts/unit.lua
+++ game/dota_addons/legion_td/scripts/vscripts/unit.lua
@@ -84,9 +84,6 @@ function Unit.GetUnitNameByID(id)
   elseif id == 120 then return "tower_humanbuilder_soundmaster"
   elseif id == 121 then return "tower_humanbuilder_spearman"
   elseif id == 122 then return "tower_humanbuilder_tactician"
-
-  elseif id == 123 then return "tower_astralbuilder_quasar"
-  elseif id == 124 then return "tower_astralbuilder_blazar"
   
       elseif id == 150 then return "tower_mechanicalbuilder_vacuum_cleaner"
       elseif id == 151 then return "tower_mechanicalbuilder_catapult"
