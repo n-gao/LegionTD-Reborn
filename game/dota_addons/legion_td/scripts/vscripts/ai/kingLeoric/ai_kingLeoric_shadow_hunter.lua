b777bc69c4f977e978b8a74846c0fcd0f09e7ef4
Doyle3694 <Doyle3694@users.noreply.github.com>
2016-11-03 13:42:30 +0100

    (King Leoric) Balance changes & Bug fix Increased average zealot scarab attack damage by 2 (14->16) Increased zealot scarab armor by 1 (0->1) Decreased randomness in mind stealer damage (25-75 -> 35-65)
    Fixed shadow hunter not using spiked carapace

--- game/dota_addons/legion_td/scripts/vscripts/ai/kingLeoric/ai_kingLeoric_shadow_hunter.lua
+++ game/dota_addons/legion_td/scripts/vscripts/ai/kingLeoric/ai_kingLeoric_shadow_hunter.lua
@@ -8,8 +8,9 @@ function InitAI( self )
 	self:SetContextThink( "init_think", function()
 		self.aiThink = aiThinkStandardSkill
 		self.CheckIfHasAggro = CheckIfHasAggro
-		self.Skill = UseSkillNoTarget
-		self.ability = self:FindAbilityByName("kingLeoric_spiked_carapace")
+		self.ability = {}
+		self.ability[1] = self:FindAbilityByName("kingLeoric_spiked_carapace")
+		self.ability[1].Skill = UseSkillNoTarget
 		self.Unstuck = Unstuck
 		self:SetContextThink( "ai_shadow_hunter.aiThink", Dynamic_Wrap( self, "aiThink" ), 0 )
 	end, 0 )
