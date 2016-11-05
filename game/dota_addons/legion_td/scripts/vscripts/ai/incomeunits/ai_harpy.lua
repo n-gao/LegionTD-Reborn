5696e5169c2725ee242a1e84144654f2f8464a20
Doyle3694 <Doyle3694@users.noreply.github.com>
2016-11-05 16:07:32 +0100

    (AI) Bug fix Fixed harpy not using ability correctly

--- game/dota_addons/legion_td/scripts/vscripts/ai/incomeunits/ai_harpy.lua
+++ game/dota_addons/legion_td/scripts/vscripts/ai/incomeunits/ai_harpy.lua
@@ -10,8 +10,8 @@ function InitAI( self )
 		self.aiThink = aiThinkStandardSkill
 		self.CheckIfHasAggro = CheckIfHasAggro
 		self.ability = {}
-		self.ability[1] = UseSkillOnTarget
-		self.ability[1].Skill = self:GetAbilityByIndex(0)
+		self.ability[1] = self:GetAbilityByIndex(0)
+		self.ability[1].Skill = UseSkillOnTarget
 		self.NextWayPoint = NextWayPoint
 		self.Unstuck = Unstuck
 		self:SetContextThink( "ai_harpy.aiThink", Dynamic_Wrap( self, "aiThink" ), 0 )
