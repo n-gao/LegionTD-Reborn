a91858968a091f4e720b418f77991f0ad624e92e
Nicholas Gao <nicholas.gao96@gmail.com>
2018-05-16 11:28:42 +0200

    updated ranking statistics to start ranks at 1 instead of 0

--- content/dota_addons/legion_td/panorama/scripts/custom_game/statistics_rankings_entry.js
+++ content/dota_addons/legion_td/panorama/scripts/custom_game/statistics_rankings_entry.js
@@ -4,6 +4,6 @@
 (function () {
     $("#Name").steamid = $.GetContextPanel().GetAttributeString("SteamID", "Missing Rank");
     $("#Avater").steamid = $.GetContextPanel().GetAttributeString("SteamID", "Missing Rank");
-    $("#Rank").text = $.GetContextPanel().GetAttributeString("Rank", "Missing Rank");
+    $("#Rank").text = parseInt($.GetContextPanel().GetAttributeString("Rank", "-1")) + 1;
     $("#Value").text = $.GetContextPanel().GetAttributeString("Value", "Missing Value");
 })();
