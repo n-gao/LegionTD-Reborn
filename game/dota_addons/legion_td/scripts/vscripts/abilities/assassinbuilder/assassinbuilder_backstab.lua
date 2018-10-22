49996526cd4d2b9cf9b3e1e9b258e138cf1c3944
Nicholas Gao <nicholas.gao96@gmail.com>
2018-10-23 00:58:53 +0100

    missing files

--- game/dota_addons/legion_td/scripts/vscripts/abilities/assassinbuilder/assassinbuilder_backstab.lua
+++ game/dota_addons/legion_td/scripts/vscripts/abilities/assassinbuilder/assassinbuilder_backstab.lua
@@ -3,9 +3,7 @@
 	Date: February 4, 2016
 	Checks if Riki is behind his target
 	Borrows heavily from bristleback.lua]]
-	
 function CheckBackstab(params)
-	
 	local ability = params.ability
 	local multiplier = ability:GetSpecialValueFor("dmg_multiplier") - 1
 	local backstabAngle = ability:GetSpecialValueFor("backstab_angle") / 2
@@ -16,24 +14,38 @@ function CheckBackstab(params)
 
 	-- Get the radian of the origin difference between the attacker and Riki. We use this to figure out at what angle the victim is at relative to Riki.
 	local origin_difference_radian = math.atan2(origin_difference.y, origin_difference.x)
-	
+
 	-- Convert the radian to degrees.
 	origin_difference_radian = origin_difference_radian * 180
 	local attacker_angle = origin_difference_radian / math.pi
 	-- Makes angle "0 to 360 degrees" as opposed to "-180 to 180 degrees" aka standard dota angles.
 	attacker_angle = attacker_angle + 180.0
-	
+
 	-- Finally, get the angle at which the victim is facing Riki.
 	local result_angle = attacker_angle - victim_angle
 	result_angle = math.abs(result_angle)
-	
+
 	-- Check for the backstab angle.
-	if result_angle >= (180 - (backstabAngle)) and result_angle <= (180 + (backstabAngle)) then 
-
+	if result_angle >= (180 - (backstabAngle)) and result_angle <= (180 + (backstabAngle)) then
 		EmitSoundOn(params.sound, params.target)
-		local particle = ParticleManager:CreateParticle(params.particle, PATTACH_ABSORIGIN_FOLLOW, params.target) 
-		ParticleManager:SetParticleControlEnt(particle, 1, params.target, PATTACH_POINT_FOLLOW, "attach_hitloc", params.target:GetAbsOrigin(), true) 
+		local particle = ParticleManager:CreateParticle(params.particle, PATTACH_ABSORIGIN_FOLLOW, params.target)
+		ParticleManager:SetParticleControlEnt(
+			particle,
+			1,
+			params.target,
+			PATTACH_POINT_FOLLOW,
+			"attach_hitloc",
+			params.target:GetAbsOrigin(),
+			true
+		)
 
-		ApplyDamage({victim = params.target, attacker = params.attacker, damage = params.damage * multiplier, damage_type = ability:GetAbilityDamageType()})
+		ApplyDamage(
+			{
+				victim = params.target,
+				attacker = params.attacker,
+				damage = params.damage * multiplier,
+				damage_type = ability:GetAbilityDamageType()
+			}
+		)
 	end
 end
--- game/dota_addons/legion_td/scripts/vscripts/ai/ai_default.lua
+++ game/dota_addons/legion_td/scripts/vscripts/ai/ai_default.lua
@@ -5,18 +5,22 @@ function Spawn(entity)
 end
 
 function InitAI(self)
-    self:SetContextThink("init_think", function()
-        self.aiThink = aiThinkStandard
-        self.NextWayPoint = NextWayPoint
-        self.Unstuck = Unstuck
-        self:Unstuck()
-        self.kv = Game.UnitKV[self:GetUnitName()]
-        if self.kv.AbilityLevel then
-            DeepPrintTable(self.kv.AbilityLevel)
-            for key, val in pairs(self.kv.AbilityLevel) do
-                self:GetAbilityByIndex(tonumber(key:sub(8, 8))):SetLevel(tonumber(val))
+    self:SetContextThink(
+        "init_think",
+        function()
+            self.aiThink = aiThinkStandard
+            self.NextWayPoint = NextWayPoint
+            self.Unstuck = Unstuck
+            self:Unstuck()
+            self.kv = Game.UnitKV[self:GetUnitName()]
+            if self.kv.AbilityLevel then
+                DeepPrintTable(self.kv.AbilityLevel)
+                for key, val in pairs(self.kv.AbilityLevel) do
+                    self:GetAbilityByIndex(tonumber(key:sub(8, 8))):SetLevel(tonumber(val))
+                end
             end
-        end
-        self:SetContextThink("ai_standard.aiThink", Dynamic_Wrap(self, "aiThink"), 0)
-    end, 0)
+            self:SetContextThink("ai_standard.aiThink", Dynamic_Wrap(self, "aiThink"), 0)
+        end,
+        0
+    )
 end
