fd1de5189441ab551d5349f9f31005a383d5f171
Nicholas Gao <nicholas.gao96@gmail.com>
2018-11-21 11:27:51 +0000

    added null check

--- game/dota_addons/legion_td/scripts/vscripts/abilities/assassinbuilder/assassinbuilder_jinada.lua
+++ game/dota_addons/legion_td/scripts/vscripts/abilities/assassinbuilder/assassinbuilder_jinada.lua
@@ -2,18 +2,23 @@
 --[[Jinada
 	Author: Pizzalol
 	Date: 1.1.2015.]]
-function Jinada( keys )
+function Jinada(keys)
 	local ability = keys.ability
 	local level = ability:GetLevel() - 1
 	local cooldown = ability:GetCooldown(level)
-	local caster = keys.caster	
+	local caster = keys.caster
 	local modifierName = "modifier_assassinbuilder_jinada_caster"
 
 	ability:StartCooldown(cooldown)
 
-	caster:RemoveModifierByName(modifierName) 
+	caster:RemoveModifierByName(modifierName)
 
-	Timers:CreateTimer(cooldown, function()
-		ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
-		end)	
+	Timers:CreateTimer(
+		cooldown,
+		function()
+			if not ability:IsNull() then
+				ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
+			end
+		end
+	)
 end
