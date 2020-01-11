ab1e39ba6652023de3c09e83e3bbd12477fd4c1f
Flam3sDOTA <30335579+Flam3sDOTA@users.noreply.github.com>
2020-01-11 22:01:34 +0300

    Relocated Modifiers
    Relocated Modifiers

--- game/dota_addons/legion_td/scripts/vscripts/abilities/waveunits/enchantress_untouchable_lua.lua
+++ game/dota_addons/legion_td/scripts/vscripts/abilities/waveunits/enchantress_untouchable_lua.lua
@@ -1,6 +1,6 @@
 enchantress_untouchable_lua = class({})
-LinkLuaModifier( "modifier_enchantress_untouchable_lua", "abilities/modifier_enchantress_untouchable_lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_enchantress_untouchable_lua_debuff", "abilities/modifier_enchantress_untouchable_lua_debuff", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_enchantress_untouchable_lua", "abilities/waveunits/modifier_enchantress_untouchable_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_enchantress_untouchable_lua_debuff", "abilities/waveunits/modifier_enchantress_untouchable_lua_debuff", LUA_MODIFIER_MOTION_NONE )
 
 --------------------------------------------------------------------------------
 -- Passive Modifier
